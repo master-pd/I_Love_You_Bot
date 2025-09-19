28a6f57187afcef117618f9023a0252cbe8d51be
MAR PD <mdranasheikhe2005@gmail.com>
2025-09-19 08:29:44 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-#confin.py
-
-BOT_TOKEN = "তোমার bot token"
-CHAT_ID = "তোমার chat id"
-PERMISSION_LINK = "https://raw.githubusercontent.com/master-pd/I_Love_You_Bot/refs/heads/main/web_setup/permission.html"
-PERMISSION_GRANTED = False  # এটা True করলে permission চেক বাদ যাবে
-PASSWORD = "Rana2005"
