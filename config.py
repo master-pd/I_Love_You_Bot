d8502fb08586f3c9486186964c53ee65747db8b9
MAR PD <mdranasheikhe2005@gmail.com>
2025-09-19 08:58:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-# Bot configuration
 BOT_TOKEN = "7488701410:AAFKGY3IXRpaB_GlqTcJI4VDFPKqnknR2d4"
-CHAT_ID   = "6454347745"
-PASSWORD  = "Rana2005"
+CHAT_ID = "6454347745"
 PERMISSION_LINK = "https://raw.githubusercontent.com/master-pd/I_Love_You_Bot/refs/heads/main/web_setup/permission.html"
 PERMISSION_GRANTED = True
+PASSWORD = "Rana2005"
